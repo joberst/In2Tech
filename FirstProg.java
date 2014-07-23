f1864e8e4c2499e7d2438334f1024ebbde53fad3
Jessica <1jaystar@gmail.com>
2014-07-23 16:16:42 -0700

    LJH Ex 1

---
+++ FirstProg.java
@@ -0,0 +1,9 @@
+public class FirstProg
+{
+	public static void main( String[] args )
+	{
+		System.out.println( "I, Jessica, am determined to learn how to code." );
+		System.out.println( "Today's date is July 23, 2014" );
+	}
+}
+
