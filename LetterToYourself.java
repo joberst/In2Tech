7ede8575e5b3267a5ea9d0141b09526477fab22c
Jessica <1jaystar@gmail.com>
2014-07-24 09:21:15 -0700

    LJH Exercises 2 & 3

---
+++ LetterToYourself.java
@@ -0,0 +1,17 @@
+public class LetterToYourself
+{
+	public static void main( String[] args )
+	{
+		System.out.println( "+--------------------------------------------------------+" );
+		System.out.println( "|                                                   #### |" );
+		System.out.println( "|                                                   #### |" );
+		System.out.println( "|                                                   #### |" );
+		System.out.println( "|                                                        |" );
+		System.out.println( "|                                                        |" );
+		System.out.println( "|                              Jessica Oberst            |" );
+		System.out.println( "|                              3 thousand Pines Rd       |" );
+		System.out.println( "|                              Calabasas CA, 97210       |" );
+		System.out.println( "|                                                        |" );
+		System.out.println( "+--------------------------------------------------------+" );
+	}
+}
