696ac59397beb797113096c18d5f2dbf44b27306
Jessica <1jaystar@gmail.com>
2014-07-28 10:22:58 -0700

    PDB Basics and Printing

---
+++ YourInitials.java
@@ -0,0 +1,13 @@
+public class YourInitials
+{
+	public static void main( String[] args )
+	{
+		System.out.println( "JJJJJ   L       OOO " );
+		System.out.println( "  J     L      O   O" );
+		System.out.println( "  J     L      O   O" );
+		System.out.println( "  J     L      O   O" );
+		System.out.println( "J J     L      O   O" );
+		System.out.println( "J J     L      O   O" );
+		System.out.println( " JJ     LLLLL   OOO " );
+	}
+}
