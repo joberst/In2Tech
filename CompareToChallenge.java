b820690fbeb35afc7bc9df86931a62bf4cac63ba
Jessica <1jaystar@gmail.com>
2014-08-04 15:21:00 -0700

    PBD If Statements - Exercise 38

---
+++ CompareToChallenge.java
@@ -0,0 +1,65 @@
+public class CompareToChallenge
+{
+	public static void main(String[] args)
+	{
+		
+
+		// results less than 0
+		System.out.print("Comparing \"apple\" with \"orange\" produces ");
+		int i = "apple".compareTo("orange");
+		System.out.println(i);
+		System.out.println();
+
+		System.out.print("Comparing \"bee\" with \"bonnet\" produces ");
+		System.out.println( "bee".compareTo("bonnet") );
+		System.out.println();
+
+		System.out.print("Comparing \"car\" with \"pool\" produces ");
+		System.out.println( "car".compareTo("pool") );
+		System.out.println();
+
+		System.out.print("Comparing \"dog\" with \"house\" produces ");
+		System.out.println( "dog".compareTo("house") );
+		System.out.println();
+
+		System.out.print("Comparing \"egg\" with \"head\" produces ");
+		System.out.println( "egg".compareTo("head") );
+		System.out.println();
+
+		// results greater than 0
+		System.out.print("Comparing \"glass\" with \"eye\" produces ");
+		System.out.println("glass".compareTo("eye") );
+		System.out.println();
+
+		System.out.print("Comparing \"slow\" with \"roll\" produces ");
+		System.out.println( "slow".compareTo("roll") );
+		System.out.println();
+
+		System.out.print("Comparing \"no\" with \"hitter\" produces ");
+		System.out.println( "no".compareTo("hitter") );
+		System.out.println();
+
+		System.out.print("Comparing \"wrong\" with \"turn\" produces ");
+		System.out.println( "wrong".compareTo("turn") );
+		System.out.println();
+
+		System.out.print("Comparing \"rag\" with \"doll\" produces ");
+		System.out.println( "rag".compareTo("doll") );
+		System.out.println();
+
+		// results 0
+		System.out.print("Comparing \"jimmy\" with \"jimmy\" produces ");
+		System.out.println( "jimmy".compareTo("jimmy") );
+		System.out.println();
+
+		System.out.print("Comparing \"week\" with \"week\" produces ");
+		System.out.println( "week".compareTo("week") );
+		System.out.println();
+
+	}
+}
+
+// http://programmingbydoing.com/a/compareto-challenge.html
+
+
+
