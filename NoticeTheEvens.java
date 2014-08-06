8e712f99b23c901c64d350f63218b3fb57e21578
Jessica <1jaystar@gmail.com>
2014-08-06 12:40:18 -0700

    PBD: For Loops - Exercise 70

---
+++ NoticeTheEvens.java
@@ -0,0 +1,22 @@
+public class NoticeTheEvens
+{
+	public static void main(String[] args)
+	{
+
+		for ( int x = 1; x <= 20; x++ )
+		{
+    		if ( x % 2 == 0 )
+    		{
+        		System.out.println( x + " <" );
+    		}
+    		else
+    		{
+        		System.out.println( x );
+    		}
+		} // end for
+
+	} // end main
+
+} // end class
+
+
