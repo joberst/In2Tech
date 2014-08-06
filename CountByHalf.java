ea021d81af431769f9a413e2427b9225c1ce6988
Jessica <1jaystar@gmail.com>
2014-08-06 12:10:50 -0700

    PBD: For Loops - Exercise 68

---
+++ CountByHalf.java
@@ -0,0 +1,17 @@
+public class CountByHalf
+{
+	public static void main(String[] args)
+	{
+		
+		System.out.println("x");
+		System.out.println("______");
+
+		for (double x = -10; x <= 10; x+=0.5)
+		{
+			System.out.println(x);
+		}
+
+	}
+}
+
+// http://programmingbydoing.com/a/counting-by-halves.html
