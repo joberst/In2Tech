ebf106c18f13c9672344942615d1b10b86641f2b
Jessica <1jaystar@gmail.com>
2014-08-06 11:31:36 -0700

    PBD: For Loops - Exercise 65

---
+++ CountingMachine.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class CountingMachine
+{
+	public static void main(String[] args)
+	{
+		Scanner keyboard = new Scanner(System.in);
+
+		int number;
+
+		System.out.println("Choose a number and I will count from 0 to that number.");
+		System.out.print("Count to: ");
+		number = keyboard.nextInt();
+
+		for (int i = 0; i <= number; i++)
+		{
+			System.out.print( i + " ");
+		}
+
+		System.out.println();
+	}
+}
+
+// http://programmingbydoing.com/a/counting-machine.html
