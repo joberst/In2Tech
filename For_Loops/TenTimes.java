01100060491da5bc33b21b23d57d30672bc7f863
Jessica <1jaystar@gmail.com>
2014-08-06 20:03:24 -0700

    PBD: For Loops - Exercise 65

---
+++ For_Loops/TenTimes.java
@@ -0,0 +1,14 @@
+public class TenTimes
+{
+	public static void main(String[] args)
+	{
+		int i;
+
+		for (i = 1; i <= 10; i++ )
+		{
+			System.out.println( i + ". Mr. Mitchell is cool.");
+		}
+	}
+}
+
+// http://programmingbydoing.com/a/ten-times.html
