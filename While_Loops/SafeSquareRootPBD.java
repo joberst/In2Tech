9764cfd96a0485cb9524fac7b959c0c8b02ebede
Jessica <1jaystar@gmail.com>
2014-08-06 22:38:36 -0700

    PBD: While Loops - Exercise 60

---
+++ While_Loops/SafeSquareRootPBD.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class SafeSquareRootPBD
+{
+	public static void main(String[] args)
+	{
+		Scanner keyboard = new Scanner(System.in);
+		int number;
+		double sqrt;
+		
+
+		System.out.println("SQUARE ROOT.");
+		System.out.print("Enter a number: ");
+		number = keyboard.nextInt();
+
+
+		while ( number < 0) 
+		{
+			
+			System.out.println("You can't take the square root of a negative number, silly goose.");
+			System.out.print("Try again: ");
+			number = keyboard.nextInt();
+		}
+
+		sqrt = Math.sqrt(number);
+		System.out.println("The square root of " + number + " is " + sqrt);
+	}
+}
+
+// http://programmingbydoing.com/a/safe-square-root.html
+// Exercise 60 
