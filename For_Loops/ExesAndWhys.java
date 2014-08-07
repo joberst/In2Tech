aec174dc1296ded60eaf164d83a2e67c4d98263b
Jessica <1jaystar@gmail.com>
2014-08-06 20:07:23 -0700

    PBD: For Loops - Exercise 69

---
+++ For_Loops/ExesAndWhys.java
@@ -0,0 +1,20 @@
+public class ExesAndWhys
+{
+	public static void main(String[] args)
+	{
+		
+		System.out.println("x\ty");
+		System.out.println("---------------");
+
+		for (double x = -10; x <= 10; x+=0.5)
+		{
+			double y = x * x;
+			System.out.println(x + "\t" + y);
+				
+		} // end for
+
+	} // end main
+
+} // end class
+
+// http://programmingbydoing.com/a/xs-and-ys.html
