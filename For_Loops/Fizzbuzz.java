802c92ca6d5383e3098aee37f438313b909d1388
Jessica <1jaystar@gmail.com>
2014-08-06 20:09:00 -0700

    PBD: For Loops - Exercise 71

---
+++ For_Loops/Fizzbuzz.java
@@ -0,0 +1,33 @@
+public class FizzBuzz
+{
+	public static void main(String[] args)
+	{
+
+		for ( int x = 1; x <= 100; x++ )
+		{
+            
+
+    		if ( x % 3 == 0 && x % 5 == 0)
+    		{
+        		System.out.println( "FizzBuzz" );
+    		}
+    		else if ( x % 5 == 0 )
+    		{
+        		System.out.println( "Buzz" );
+    		}
+            else if ( x % 3 == 0 )
+            {   
+                System.out.println( "Fizz" );
+            }
+            else
+            {
+                System.out.println( x );
+            }
+
+		} // end for
+
+	} // end main
+
+} // end class
+
+// http://programmingbydoing.com/a/fizzbuzz.html
