019176a1b7abb6f610a4ae4ca8d97a4c233e792b
Jessica <1jaystar@gmail.com>
2014-08-06 20:15:50 -0700

    PBD: Do-While Loops - Exercise 58

---
+++ Do_While/DiceDoublesDoWhile.java
@@ -0,0 +1,36 @@
+import java.util.Random;
+
+public class DiceDoublesDoWhile
+{
+	public static void main(String[] args)
+	{
+		Random rando = new Random();
+
+		System.out.println("HERE COME THE DICE!");
+		System.out.println();
+
+		int die1 = 1 + rando.nextInt(6);
+		int die2 = 1 + rando.nextInt(6);
+
+		do {
+			System.out.println("Roll #1: " + die1);
+			System.out.println("Roll #2: " + die2);
+			System.out.println("The total is " + (die1 + die2) + "!");
+			System.out.println();
+			die1 = 1 + rando.nextInt(6);
+			die2 = 1 + rando.nextInt(6);
+		} while (die1 != die2);
+
+
+		System.out.println("Roll #1: " + die1);
+		System.out.println("Roll #2: " + die2);
+		System.out.println("The total is " + (die1 + die2) + "!");
+		System.out.println();
+
+
+	} // end main
+
+} // end class
+
+// http://programmingbydoing.com/a/shorter-double-dice.html
+// Exercise 58
