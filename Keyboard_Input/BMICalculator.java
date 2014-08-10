29badd0d887be7245fe3dbc4d28c4f71d4b064d5
Jessica <1jaystar@gmail.com>
2014-08-09 20:48:12 -0700

    PBD: Keyboard Input - Exercise 20

---
+++ Keyboard_Input/BMICalculator.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class BMICalculator
+{
+	public static void main( String[] args )
+	{
+		Scanner keyboard = new Scanner(System.in);
+		
+		double heightInFeet;
+		double heightInInches;
+		double weightInPounds;
+			
+		System.out.print("Your height in feet: ");
+		heightInFeet = keyboard.nextDouble();
+		
+		System.out.print("Your height in inches: ");
+		heightInInches = keyboard.nextDouble();
+		
+		System.out.print("Your weight in lbs: ");
+		weightInPounds = keyboard.nextDouble();
+		
+		
+		//BMI = weight in kilos divided by the square of height in meters
+		System.out.println
+		("Your BMI is " + 
+		
+		// Convert weight to kilos
+			(
+				( weightInPounds / 2.2046) / 
+		
+				// Convert inches to meters
+					(
+						(((heightInFeet * 12) + heightInInches) / 39.370 ) * 
+		
+					//multiply by itself to get square
+						(((heightInFeet * 12) + heightInInches) / 39.370 )
+					) // end convert inches to meters
+			)
+		);
+		
+	} // end main
+} // end class
+
+
+
+
+
+// http://programmingbydoing.com/a/bmi-calculator.html
+// PBD: Keyboard Input - Exercise 20
