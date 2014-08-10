cb2b090d9d31f9293503e0e63537a87074a52d40
Jessica <1jaystar@gmail.com>
2014-08-09 21:04:34 -0700

    PBD: Basics and Printing - Exercise 5

---
+++ Basics_and_Printing/CommentsAndSlashes.java
@@ -0,0 +1,22 @@
+// Jessica Oberst 7.26.14
+// I was correct about the two slashes. I knew they were comments. Just like in CSS.
+public class CommentsAndSlashes
+{
+	public static void main( String[] args )
+	{
+		// A comment, this is so you can read your program later.
+		// Anything after the // is ignored by Java.
+		
+		System.out.println( "I could have code like this." ); //and the comment after is ignored.
+		
+		// You can also use a comment to "disable" or comment out a piece of code:
+		// System.out.println( "This won't run.");
+		
+		System.out.println( "This will run." );
+	}
+}
+
+/*
+http://programmingbydoing.com/a/comments-and-slashes.html
+PBD: Basics and Printing - Exercise 5
+*/
