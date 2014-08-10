2066bfd88bd014e9f6f29fb27f0426e4cf2b6e42
Jessica <1jaystar@gmail.com>
2014-08-09 20:35:59 -0700

    PBD: Variables - Exercise 11

---
+++ Variables/UsingVariables.java
@@ -0,0 +1,19 @@
+public class UsingVariables
+{
+	public static void main( String[] args )
+	{
+		int room;
+		double number;
+		String subject;
+		
+		room = 113;
+		number = 2.71828;
+		subject = "Computer Science";
+		
+		System.out.println( "This is room # " + room );
+		System.out.println( "e is close to " + number );
+		System.out.println( "I am learning a bit about " + subject);
+	}
+}
+
+// http://programmingbydoing.com/a/using-variables.html
