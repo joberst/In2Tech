d08f5a380c1d2d8f501109950b6a4b6c6a9b9274
Jessica <1jaystar@gmail.com>
2014-08-09 20:55:52 -0700

    PBD: Basics and Printing - Exercise 8

---
+++ Basics_and_Printing/NumbersAndMath.java
@@ -0,0 +1,47 @@
+public class NumbersAndMath
+{
+	public static void main( String[] args )
+	{
+		// Printing String
+		System.out.println( "I will now count my chickens:" );
+		
+		// Calculating expression
+		System.out.println( "Hens " + ( 25.0 + 30.0 / 6.0 ) );
+		
+		// Calculating the expression
+		System.out.println( "Roosters " + ( 100.0 - 25.0 * 3.0 % 4.0 ) );
+		
+		// Printing String
+		System.out.println( "Now I will count the eggs:" );
+		
+		// Calculating the expression
+		System.out.println( 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0 );
+		
+		// Printing String
+		System.out.println( "Is it true that 3.0 + 2.0 < 5.0 - 7.0?" );
+		
+		// Calculating the expression
+		System.out.println( 3.0 + 2.0 < 7.0 - 7.0 );
+		
+		// Printing String and calculating the expression
+		System.out.println( "What is 3.0 + 2.0? " + (3.0 + 2.0 ) );
+		
+		// Printing String and calculating the expression
+		System.out.println( "What is 5.0 - 7.0? " + ( 5.0 - 7.0 ) );
+		
+		// Printing String
+		System.out.println( "Oh, that's why it's false." );
+		
+		// Printing String
+		System.out.println( "How about some more." );
+		
+		// Printing String and calculating the expression to return true or false
+		System.out.println( "Is it greater? " + ( 5.0 > -2.0 ) );
+		
+		// Printing String and calculating the expression to return true or false
+		System.out.println( "Is it greater or equal? " + ( 5.0 >= -2.0 ) );
+		
+		// Printing String and calculating the expression to return true or false
+		System.out.println( "Is it less or equal? " + ( 5.0 <= -2.0 ) );
+	}
+}
